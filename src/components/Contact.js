32f3ca784c4ea8c88772a232de8069d993b93d94
karan-mandhre <2002karanmandhre@gmail.com>
2024-01-18 14:04:43 +0530

    Complete Project

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,10 +2,8 @@ const Contact = () => {
   return (
     <>
       <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
-        
-
         <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-        <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900 mb-10">
+          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900 mb-10">
             Connect With Us
           </h2>
           <div>
